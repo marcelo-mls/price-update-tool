eab3db9e6a527ebb69666376268a5652086f12de
marcelo marques <marcelo-mls@hotmail.com>
2023-05-15 18:15:03 -0300

    feat(fullstack): render data from backend to frontend

--- backend/src/models/database/connection.js
+++ backend/src/models/database/connection.js
@@ -7,7 +7,7 @@ const dbConnection = mysql.createPool({
 	user: process.env.MYSQL_USER,
 	password: process.env.MYSQL_PASSWORD,
 	database: process.env.MYSQL_DATABASE,
-	multipleStatements: process.env.MULTIPLE_STATEMENTS,
+	multipleStatements: process.env.MYSQL_MULTIPLE_STATEMENTS,
 });
 
 module.exports = dbConnection;
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,6 @@ import API from './axios';
 export async function fetchAndValidateProducts(payload) {
   try {
     const response = await API.post('products/', payload);
-
     return response;
   } catch (error) {
     console.error(`${error.name}: ${error.message}`);
