48ef87e391c7a1b784d37a6c09aa5242fba1fbf3
marcelo marques <marcelo-mls@hotmail.com>
2023-05-15 17:20:19 -0300

    feat(frontend): start the frontend
    install dependencies and configure API fetching with axios

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1>Ferramenta de Atualizar Preço</h1>
+  )
+}
+
+export default App;
---
+++ frontend/src/utils/api.js
@@ -0,0 +1,12 @@
+import API from './axios';
+
+export async function fetchAndValidateProducts(payload) {
+  try {
+    const response = await API.post('products/', payload);
+
+    return response;
+  } catch (error) {
+    console.error(`${error.name}: ${error.message}`);
+    return [];
+  }
+}
