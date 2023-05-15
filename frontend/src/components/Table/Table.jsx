c4a2f87623bf4ffea5d2a3b6aa6745883cbcdd3c
marcelo marques <marcelo-mls@hotmail.com>
2023-05-15 18:27:20 -0300

    feat(frontend): adds logic to load and read CSV
    uses the paparse lib to read the CSV file and implements some validation rules about the file
    structure, to enable and disable validate button

--- frontend/src/components/Table/Table.jsx
+++ frontend/src/components/Table/Table.jsx
@@ -1,37 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { fetchAndValidateProducts } from '../../utils/api';
+import csvReader from '../../utils/csvReader';
 
 export default function Table() {
-  const [tableData, setTableData] = useState([])
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isInvalidFile, setIsInvalidFile] = useState(true);
+  const [tableData, setTableData] = useState(null)
+
+  const handleFileSelect = async (event) => {
+    const file = event.target.files[0]
+    const csvData = await csvReader(file)
+    setSelectedFile(csvData)
+  };
+
+  const handleFileValidation = () => {
+    if (!selectedFile) {
+      setIsInvalidFile(true)
+    } else if (selectedFile.headers.length !== 2
+        || selectedFile.data.length === 0
+        || !selectedFile.headers.includes('product_code')
+        || !selectedFile.headers.includes('new_price')
+      ) {
+      setIsInvalidFile(true)
+    } else {
+      setIsInvalidFile(false)
+    }
+  };
 
   const fetchApi = async () => {
     const payload = 'payload'
     const response = await fetchAndValidateProducts({payload});
-    setTableData(response.data)
+    setTableData(response)
   };
+
+  useEffect(() => {
+    handleFileValidation()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedFile]);
 
   return (
     <div>
 
       <h1>Ferramenta de Atualizar Preço</h1>
-      <input
-        type="file"
-        accept=".csv"
-        onChange={()=>{}}
-      />
-      <button
-        type='button'
-        disabled={false}
-        onClick={fetchApi}
-      >
+      <input type="file" accept=".csv" onChange={handleFileSelect}/>
+      <button type='button' disabled={isInvalidFile} onClick={fetchApi}>
         Validar
       </button>
-      <button
-        type='button'
-        disabled={false}
-        onClick={()=>{}}
-      >
+      <button type='button' disabled={false} onClick={()=>{}}>
         Atualizar
       </button>
       
---
+++ frontend/src/utils/csvReader.js
@@ -0,0 +1,14 @@
+import Papa from 'papaparse';
+
+export default function csvReader(csvFile) {
+  return new Promise((resolve) => {
+    const configObject = {
+      header: true,
+      skipEmptyLines: true,
+      complete: (result) => resolve({data: result.data, headers: result.meta.fields})
+    }
+
+    Papa.parse(csvFile, configObject)
+  })
+}
+
